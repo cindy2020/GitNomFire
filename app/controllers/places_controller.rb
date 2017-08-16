32b20168a391852aa2e0cf800e1b98c759154248
cindy2020 <coharastudent@gmail.com>
2017-08-16 16:04:59 +0000

    Details Page works

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,9 +15,10 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
-  def show    
+  def show
+    @place = Place.find(params[:id])  
   end
-  
+
   private
 
   def place_params
